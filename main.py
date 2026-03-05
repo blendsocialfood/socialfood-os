897f2a1334d542c8b640ecb84cfad488b68baff0
Blend <blend@MacBook-Air-de-Blend.local>
2026-03-05 01:27:26 -0300

    Social Food OS - landing page

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/admin')
+def admin():
+    return send_from_directory('.', 'admin.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
